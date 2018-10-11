d664e4236e9c4ce8e1045ff7c7e7ae2a88679576
showzen <showzen97@gmail.com>
2018-10-12 00:41:40 +0100

    initial commit

---
+++ src/main/java/com/example/Criptocurrency/CriptocurrencyApplication.java
@@ -0,0 +1,12 @@
+package com.example.Criptocurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptocurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriptocurrencyApplication.class, args);
+	}
+}
